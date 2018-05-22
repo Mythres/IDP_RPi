cda3c1e4e8621b910cbdadd7c19292064f411ce5
Yannick Kooistra <yannick.kooistra@gmail.com>
2018-05-22 12:30:42 +0200

    Fixed read command

--- interfaces/constants.py
+++ interfaces/constants.py
@@ -1,2 +1,2 @@
 bl_mac_address = "98:D3:32:21:29:67"
-commands = ["load/2", "unload/1", "start/1", "stop/1", "send/2", "receive/1", "help/1", "quit/1", "exit/1"]
+commands = ["load/2", "unload/1", "start/1", "stop/1", "send/2", "read/1", "help/1", "quit/1", "exit/1"]
